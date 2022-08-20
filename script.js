d23540d6c174937a1da44549011e20dd1f6bcd3a
rajesh <krajeshgowri7032@mail.com>
2022-08-20 22:15:25 +0530

    Javascript-Remove

---
+++ script.js
@@ -0,0 +1,3 @@
+const element = document.getElementsByTagName("p");
+document.getElementById("demo").innerHTML='javascript is an object-based language' + element[0].innerHTML;
+document.getElementById("demo").style.color = "yellow";
